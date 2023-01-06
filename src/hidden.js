a1db3d267583b009495a8cc5e3ecdb3bf2e9756a
bijayneupane18 <bjneupane5@gmail.com>
2023-01-06 19:00:47 +0545

    started

---
+++ src/hidden.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const hidden = () => {
+  function randomNumber() {
+    return Math.floor(Math.random() * 5);
+  }
+  //   console.log(Math.floor(Math.random() * 5));
+  return (
+    <>
+      <h1>{randomNumber()}</h1>
+    </>
+  );
+};
+
+export default hidden;
