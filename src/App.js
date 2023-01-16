a9aa4f0dead3b0b1558f1d989292fc33583a06c9
bijayneupane18 <bjneupane5@gmail.com>
2023-01-16 14:10:29 +0545

    error aayo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-import Hidden from "./hidden";
 import UserInput from "./userInput";
 
 function App() {
   return (
     <div>
-      <Hidden />
       <UserInput />
-      <button>submit</button>
     </div>
   );
 }
--- src/userInput.js
+++ src/userInput.js
@@ -1,15 +1,33 @@
 import React, { useState } from "react";
 
 const UserInput = () => {
-  const [message, setMessage] = useState(null);
+  const [message, setMessage] = useState("");
+  // const [check, setCheck] = useState(false);
+
+  const randomNumber = () => {
+    return Math.floor(Math.random() * 5);
+  };
 
   const changeHandler = (e) => {
     setMessage(e.target.value);
+    console.log(message);
   };
+
+  const compare = () => {
+    if (randomNumber() == message) {
+      console.log("Winner");
+    } else {
+      console.log("Looser");
+    }
+  };
+
   return (
     <div>
+      <h1>{randomNumber()}</h1>
       <label>Guess the number : </label>
       <input type="number" value={message} onChange={changeHandler}></input>
+      <button onClick={compare}>Submit</button>
+      {/* {check ? compare() : ""} */}
     </div>
   );
 };
